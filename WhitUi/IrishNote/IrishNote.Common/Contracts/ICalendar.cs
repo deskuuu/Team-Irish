17404ecaf0aee16d6db4e51773dace28a739c86a
SimeonGerginov <simeongerginov@abv.bg>
2017-01-07 20:56:04 +0200

    Added ITask and ICalendar interfaces.Also added Task class.

---
+++ WhitUi/IrishNote/IrishNote.Common/Contracts/ICalendar.cs
@@ -0,0 +1,11 @@
+﻿namespace IrishNote.Common.Contracts
+{
+    using System;
+    using System.Collections.Generic;
+
+    public interface ICalendar
+    {
+        DateTime CurrentDate { get; }
+        List<ITask> tasks { get; set; }
+    }
+}
---
+++ WhitUi/IrishNote/IrishNote.Common/Contracts/ITask.cs
@@ -0,0 +1,10 @@
+﻿namespace IrishNote.Common.Contracts
+{
+    using System;
+
+    public interface ITask
+    {
+        string NameOfTask { get; set; }
+        DateTime DateAndTimeOfTask { get; set; }
+    }
+}
