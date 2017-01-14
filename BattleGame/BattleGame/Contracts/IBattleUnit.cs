856bbd922c4b470f0434dfbe65d24b7f27e493c6
Vlaev <vlado.vlaev@gmail.com>
2017-01-14 15:04:07 +0200

    Added constants, and some inheritance fixes

---
+++ BattleGame/BattleGame/Contracts/IBattleUnit.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BattleGame.Common.Enums;
+
+namespace BattleGame.Contracts
+{
+    public interface IBattleUnit
+    {
+        int Health { get; }
+
+        int Attack { get; }
+
+        int Defense { get; }
+
+    }
+}
---
+++ BattleGame/BattleGame/Models/Pikeman.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BattleGame.Common.Enums;
+using BattleGame.Contracts;
+
+namespace BattleGame.Models
+{
+    public class Pikeman : ForceUnit
+    {        
+        TypeOfAttack typeOfAttack;
+
+        public Pikeman(string name, int attack, int defense, int health) : base(name,attack,defense,health)
+        {
+          
+            this.typeOfAttack = TypeOfAttack.MiddleRangeAttack;
+        }        
+
+        public TypeOfAttack TypeOfAttack
+        {
+            get
+            {
+                return this.typeOfAttack;
+            }
+        }
+
+        public override string ToString()
+        {
+            // TODO:
+            return base.ToString();
+        }
+    }
+}
