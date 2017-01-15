a652762bf9f97ba582892a4175e9d5233437d512
vgutev <v.gutev36@gmail.com>
2017-01-16 00:05:08 +0200

    Added music player.

---
+++ BattleGame/BattleGame/Common/MusicPlayer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleGame.Common
+{
+    class MusicPlayer
+    {
+        private SoundPlayer player;
+
+        public SoundPlayer Player
+        {
+            get
+            {
+                return this.player;
+            }
+            private set
+            {
+                this.player = value;
+            }
+        }
+
+        public MusicPlayer (string fileLocation)
+        {
+            this.Player = new SoundPlayer(fileLocation);
+        }
+
+        public void Play()
+        {
+            this.Player.Play();
+        }
+        public void PlayLooping()
+        {
+            this.Player.PlayLooping();
+        }
+    }
+}
--- BattleGame/BattleGame/Startup.cs
+++ BattleGame/BattleGame/Startup.cs
@@ -3,11 +3,14 @@
     using System;
     using Contracts;
     using Engine;
+    using Common;
 
     public class Startup
     {
         public static void Main()
         {
+            MusicPlayer backgroundMusicPlayer = new MusicPlayer(".\\Media\\music.wav");
+            backgroundMusicPlayer.PlayLooping();
             IEngine engine = new GameEngine();
             engine.OnEngineMessageEvent += (message) => Console.WriteLine(message);
             engine.Start();
